b53db6837d729aa3cb70435f9ee606bc23c898f5
pblake142 <114369146+pblake142@users.noreply.github.com>
2023-02-18 18:57:31 -0500

    Create ExportListConfiguration.ps1

---
+++ ExportListConfiguration.ps1
@@ -0,0 +1,38 @@
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+#Configuration Parameters
+$SiteURL="http://url.com/"
+$ListName= "theList"
+$XMLPath="C:\Temp\list.xml"
+
+#Get the Web and List
+$Web = Get-SPWeb $SiteURL
+$List = $Web.Lists.TryGetList ($ListName)
+
+#Create an XMLTextWriter object
+$XMLWriter = New-Object System.XMl.XmlTextWriter($XMLPath,$Null)
+
+#Write XML declaration
+$XMLWriter.WriteStartDocument()
+
+#Write root element
+$XMLWriter.WriteStartElement("Fields")
+
+#Iterate through each field in the list
+foreach ($Field in $List.Fields) {
+    #Write field element with attributes
+    $XMLWriter.WriteStartElement("Field")
+    $XMLWriter.WriteAttributeString("Title", $Field.Title)
+    $XMLWriter.WriteAttributeString("InternalName", $Field.InternalName)
+    $XMLWriter.WriteAttributeString("Type", $Field.Type)
+    $XMLWriter.WriteEndElement()
+}
+
+#Write end of root element
+$XMLWriter.WriteEndElement()
+
+#Write end of XML document
+$XMLWriter.WriteEndDocument()
+
+#Close the XMLTextWriter object
+$XMLWriter.Close()
